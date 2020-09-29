91c0bf3dcef6bb9e6f1ab71999ce589e369d3ccc
Alena Nidilko <alenkan96@gmail.com>
2020-09-29 21:25:57 +0300

    add task 2

---
+++ findContour/findContour/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ex1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 738);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(827, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Новый";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(827, 135);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Выделить границу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 772);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Рисователь";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ findContour/findContour/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Windows.Input;
+
+namespace ex1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			colorDialog1.Color = left.Color;
+			new_image();
+		}
+
+		private Pen right = new Pen(Color.White);
+		private Pen left = new Pen(Color.Black);
+		private Point start;
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			start = e.Location;
+		}
+
+		//Рисование
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			Bitmap im = (Bitmap)pictureBox1.Image;
+			if (e.Button == MouseButtons.Left)
+			{
+				using (Graphics g = Graphics.FromImage(im))
+					g.DrawLine(left, start, e.Location);
+				start = e.Location;
+			}
+			else if (e.Button == MouseButtons.Right)
+			{
+				using (Graphics g = Graphics.FromImage(im))
+					g.DrawLine(right, start, e.Location);
+				start = e.Location;
+			}
+			pictureBox1.Image = im;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			new_image();
+		}
+
+		private void new_image()
+		{
+			Bitmap im = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			using (Graphics g = Graphics.FromImage(im))
+				g.Clear(Color.White);
+			
+			if (pictureBox1.Image != null)
+				pictureBox1.Image.Dispose();
+			pictureBox1.Image = im;
+		}
+
+		private Bitmap img;
+		private void button4_Click(object sender, EventArgs e)
+		{
+			img = (Bitmap)pictureBox1.Image;
+			List<Point> points = find_contour();
+			foreach (Point p in points)
+				img.SetPixel(p.X, p.Y, Color.Red);  //Смена цвета выделенной границы 
+			pictureBox1.Image = img;
+		}
+
+		//Поиск правой верхней точки области
+		private Point find_right_top(Color start)
+		{
+			for (int i = img.Width - 1; i >= 0; i--)
+				for (int j = 0; j < img.Height; j++)
+					if (start != img.GetPixel(i, j))
+						return new Point(i, j);
+			return new Point(img.Width / 2, img.Height / 2);
+		}
+
+		//Смена направления движения по границе связанной области
+		private Point change_direction(Point cur, int dir)
+		{
+			switch (dir)
+			{
+				case 0: return new Point(cur.X + 1, cur.Y);
+				case 1: return new Point(cur.X + 1, cur.Y - 1);
+				case 2: return new Point(cur.X, cur.Y - 1);
+				case 3: return new Point(cur.X - 1, cur.Y - 1);
+				case 4: return new Point(cur.X - 1, cur.Y);
+				case 5: return new Point(cur.X - 1, cur.Y + 1);
+				case 6: return new Point(cur.X, cur.Y + 1);
+				case 7: return new Point(cur.X + 1, cur.Y + 1);
+				default: return cur;
+			}
+		}
+
+		private int dir;
+		private Point next_point(Point cur, Color col)
+		{
+			int start_dir = (dir + 2) % 8;
+			Point next_p = change_direction(cur, start_dir);
+			int new_dir = start_dir;
+			//пока цвет следующего пикселя не цвета границы
+			while (img.GetPixel(next_p.X, next_p.Y) != col)
+			{
+				if (new_dir < 0)
+					new_dir += 8;
+				next_p = change_direction(cur, new_dir);
+				new_dir--;
+			}
+			dir = new_dir;
+			return next_p;
+		}
+
+		//Поиск границы связанной области
+		private List<Point> find_contour()
+		{
+			List<Point> points = new List<Point>();
+			Point start_point = find_right_top(img.GetPixel(img.Width - 1, 0));
+			points.Add(start_point);
+			dir = 6;
+
+			Color col = img.GetPixel(start_point.X, start_point.Y);
+			Point next_p = next_point(start_point, col);
+			points.Add(next_p);
+			while (next_p != start_point)
+			{
+				next_p = next_point(next_p, col);
+				points.Add(next_p);
+			}
+			return points;
+		}
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

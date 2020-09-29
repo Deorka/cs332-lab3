594247146fc68fa25851c05ff2f07b812b71e779
Monluxfer <shibarome@gmail.com>
2020-09-29 17:49:10 +0300

    add task 3

---
+++ lineDrawing/lineDrawing/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace lineDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 445);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(139, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Алгоритм Брезенхэма";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 469);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(92, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Алгоритм ВУ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Очистить поле";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lineDrawing/lineDrawing/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lineDrawing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Random rnd = new Random();
+        Color color;
+        bool firstPoint = false;
+        bool bres = true;
+        bool wu = false;
+        int x0 = -1;
+        int y0 = -1;
+        int x1 = -1;
+        int y1 = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            int wdth = pictureBox1.Width;
+            int hght = pictureBox1.Height;
+            bmp = new Bitmap(wdth, hght);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        public void Bresenham_line_algorithm(int x0, int y0, int x1, int y1)
+        {
+            var bad = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            // модифицируем координаты точек, чтобы не было проблем с вычислениями
+            // слишком большой угол наклона
+            if (bad)
+            {
+                (x0, y0) = (y0, x0);
+                (x1, y1) = (y1, x1);
+            }
+            // линия растёт не слева направо
+            if (x0 > x1)
+            {
+                (x0, x1) = (x1, x0);
+                (y0, y1) = (y1, y0);
+            }
+
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+            // вертикальное расстояние между текущим y и точным y для текущего x, с домножением на dx
+            // чтобы каждый шаг ошибка изменялась на dy
+            int error = dx / 2;
+
+            // направление роста y
+            int ystep = (y0 < y1) ? 1 : -1;
+            int y = y0;
+            for (int x = x0; x <= x1; x++)
+            {
+                if (bad)
+                {
+                    (x, y) = (y, x);
+                }
+                // возвращаем координаты на свои места, чтобы нарисовать отрезок корректно
+                bmp.SetPixel(x, y, color);
+                if (bad)
+                {
+                    (x, y) = (y, x);
+                }
+                // когда ошибка пересекает рубеж, новый пиксел сдвигается выше
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+        }
+        public void WuLine(int x0, int y0, int x1, int y1)
+        {
+            var steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                (x0, y0) = (y0, x0);
+                (x1, y1) = (y1, x1);
+            }
+            if (x0 > x1)
+            {
+                (x0, x1) = (x1, x0);
+                (y0, y1) = (y1, y0);
+            }
+
+            DrawPoint(steep, x0, y0, 1); // Эта функция автоматом меняет координаты местами в зависимости от переменной steep
+            DrawPoint(steep, x1, y1, 1); // Последний аргумент — интенсивность в долях единицы
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+            float gradient = dy / dx;
+            float y = y0 + gradient;
+            for (var x = x0 + 1; x <= x1 - 1; x++)
+            {
+                DrawPoint(steep, x, (int)y, (int)((1 - (y - (int)y)) * 255));
+                DrawPoint(steep, x, (int)y + 1, (int)((y - (int)y) * 255));
+                y += gradient;
+            }
+        }
+
+        public void DrawPoint(bool steep, int x, int y, int alpha)
+        {
+            if (steep)
+            {
+                (x, y) = (y, x);
+            }
+            Color color = Color.FromArgb(alpha, 0, 0, 0);
+            bmp.SetPixel(x, y, color);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Image = bmp;
+            color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+            if (firstPoint == false)
+            {
+                x0 = e.X;
+                y0 = e.Y;
+                firstPoint = true;
+            }
+            else if (bres == true)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                Bresenham_line_algorithm(x0, y0, x1, y1);
+                firstPoint = false;
+            }
+            else if (wu == true)
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                WuLine(x0, y0, x1, y1);
+                firstPoint = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            bres = true;
+            wu = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            bres = false;
+            wu = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int wdth = pictureBox1.Width;
+            int hght = pictureBox1.Height;
+            bmp = new Bitmap(wdth, hght);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+    }
+}
